826c362a4eaf58ab372ec0972c1f47ba81a67f2c
fankun <fankun@51h5.com>
2020-03-24 23:17:24 +0800

    add taobao.top.auth.token.create api

---
+++ request/taobao_top_auth_token_create.go
@@ -0,0 +1,32 @@
+package request
+
+import (
+    "51h5.com/sdk/top/internal/utils"
+    "net/url"
+)
+
+type TaobaoTopAuthTokenCreateRequest struct {
+    topRequest
+    Code string `json:"code"`
+    Uuid string `json:"uuid"`
+}
+
+func (r *TaobaoTopAuthTokenCreateRequest) Method() string {
+    return "taobao.top.auth.token.create"
+}
+
+func (r *TaobaoTopAuthTokenCreateRequest) Values() url.Values {
+    return url.Values{
+        "code": {r.Code},
+        "uuid": {r.Uuid},
+    }
+}
+
+func (r *TaobaoTopAuthTokenCreateRequest) Check() (code uint, err error) {
+    code, err = utils.CheckNotEmpty("code", r.Code)
+    if err != nil {
+        return
+    }
+
+    return
+}
